941096884776fedb8f10dd64fd3bc350d55d46c6
spanishprisoner <jacek.mowinski@gmail.com>
2018-06-17 23:55:55 +0200

    Using ViewComponent for title

---
+++ src/LastSeenWeb.Front/Pages/Components/Title/Title.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace LastSeenWeb.Front.Pages.Components.Title
+{
+	public class Title : ViewComponent
+	{
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			return View();
+		}
+	}
+}
