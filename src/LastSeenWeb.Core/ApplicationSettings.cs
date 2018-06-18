93c8ec25b7b64d52c1e4026e1f4254e913b93e04
spanishprisoner <jacek.mowinski@gmail.com>
2018-06-18 18:13:12 +0200

    Adding ApplicationSettings

---
+++ src/LastSeenWeb.Core/ApplicationSettings.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace LastSeenWeb.Core
+{
+	public class ApplicationSettings
+	{
+		public string ApplicationName { get; set; } = "Last Seen";
+		public List<(string, string)> NavBarLoggedOut { get; set; } = new List<(string, string)>
+		{
+			("/Auth/Register","Register"),
+			("/Auth/Login","Log in")
+		};
+		public List<(string, string)> NavBarLoggedIn { get; set; } = new List<(string, string)>
+		{
+			("/Auth/Logout","Log out")
+		};
+	}
+}
