8a9aaf353411ab3997a23bec7b51b3a64c14d819
spanishprisoner <jacek.mowinski@gmail.com>
2018-06-18 18:14:14 +0200

    Adding NavBar ViewComponent

---
+++ src/LastSeenWeb.Front/Pages/Components/NavBar/NavBar.cs
@@ -0,0 +1,14 @@
+﻿using LastSeenWeb.Core;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace LastSeenWeb.Front.Pages.Components.NavBar
+{
+	public class NavBar : ViewComponent
+	{
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			return View();
+		}
+	}
+}
