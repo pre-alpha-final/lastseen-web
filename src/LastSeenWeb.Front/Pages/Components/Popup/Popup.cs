5c1a32c17a88b4b65384075d1a5774861c748213
spanishprisoner <jacek.mowinski@gmail.com>
2018-06-22 17:00:34 +0200

    Basic files

---
+++ src/LastSeenWeb.Front/Pages/Components/Popup/Popup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace LastSeenWeb.Front.Pages.Components.Popup
+{
+	public class Popup : ViewComponent
+	{
+		public async Task<IViewComponentResult> InvokeAsync()
+		{
+			return View();
+		}
+	}
+}
