67e49dcf8d837a0c179b793182597f8800b79431
spanishprisoner <jacek.mowinski@gmail.com>
2018-07-23 00:38:47 +0200

    Adding custom tooltip

---
+++ src/LastSeenWeb.Front/Pages/Components/Tooltip/Tooltip.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace LastSeenWeb.Front.Pages.Components.Tooltip
+{
+	public class Tooltip : ViewComponent
+	{
+		public Task<IViewComponentResult> InvokeAsync(TooltipModel model)
+		{
+			return Task.FromResult<IViewComponentResult>(View(model));
+		}
+	}
+}
---
+++ src/LastSeenWeb.Front/Pages/Components/Tooltip/TooltipModel.cs
@@ -0,0 +1,9 @@
+﻿namespace LastSeenWeb.Front.Pages.Components.Tooltip
+{
+	public class TooltipModel
+	{
+		public string Name { get; set; }
+		public string Text { get; set; }
+		public string Position { get; set; }
+	}
+}
