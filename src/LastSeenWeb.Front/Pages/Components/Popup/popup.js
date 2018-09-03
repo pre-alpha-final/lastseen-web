2198a4528876ddb1fe889d9a732fddb4d8bf45b9
spanishprisoner <jacek.mowinski@gmail.com>
2018-09-03 19:57:42 +0200

    Minor fixes

--- src/LastSeenWeb.Front/Pages/Components/Popup/popup.js
+++ src/LastSeenWeb.Front/Pages/Components/Popup/popup.js
@@ -1,23 +1,23 @@
 ﻿var _popup = {};
 
 (popup => {
-	var modal = document.getElementById('popup');
-	window.onclick = function (event) {
+	var modal = document.getElementById("popup");
+	window.onclick = function(event) {
 		if (event.target == modal) {
 			popup.close();
 		}
-	}
+	};
 
 	popup.open = async id => {
-		await ajaxGetAndReplace('/PopupContent/' + id, '#popupContent');
+		await ajaxGetAndReplace(`/PopupContent/${id}`, "#popupContent");
 		modal.style.display = "flex";
-	}
+	};
 
 	popup.update = async data => {
-		await ajaxPost('/PopupContent/', data, () => { });
-	}
+		await ajaxPost("/PopupContent/", data, () => {});
+	};
 
 	popup.close = () => {
 		modal.style.display = "none";
-	}
+	};
 })(_popup);
