0d1bc7122ab3dae1385e78e2946e7eec58b33c06
spanishprisoner <jacek.mowinski@gmail.com>
2019-01-09 19:31:02 +0100

    Adding basic masonry-list.component

--- src/LastSeenWeb.AngularFront/ClientApp/src/app/app.module.ts
+++ src/LastSeenWeb.AngularFront/ClientApp/src/app/app.module.ts
@@ -4,9 +4,11 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 
+import { JQUERY_PROVIDER } from './jquery.service';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar.component';
-import { JQUERY_PROVIDER } from './jquery.service';
+import { MasonryListComponent } from './lastseen-items/masonry-list.component';
+import { LastseenItemsModule } from './lastseen-items/lastseen-items.module';
 
 @NgModule({
   declarations: [
@@ -17,7 +19,9 @@ import { JQUERY_PROVIDER } from './jquery.service';
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
+    LastseenItemsModule,
     RouterModule.forRoot([
+      { path: '', component: MasonryListComponent, pathMatch: 'full' },
     ])
   ],
   providers: [JQUERY_PROVIDER],
---
+++ src/LastSeenWeb.AngularFront/ClientApp/src/app/lastseen-items/masonry-list.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './masonry-list.component.html',
+  styleUrls: ['./masonry-list.component.css']
+})
+export class MasonryListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
