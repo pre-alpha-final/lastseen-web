960ca7de6ee26e3a70c57f6359f28fda1e4c562e
spanishprisoner <jacek.mowinski@gmail.com>
2019-01-09 19:31:02 +0100

    Adding basic masonry-list.component

---
+++ src/LastSeenWeb.AngularFront/ClientApp/src/app/lastseen-items/lastseen-items.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MasonryListComponent } from './masonry-list.component';
+
+@NgModule({
+  declarations: [MasonryListComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class LastseenItemsModule { }
