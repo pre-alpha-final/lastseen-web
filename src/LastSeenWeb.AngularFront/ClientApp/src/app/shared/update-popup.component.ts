95fa0b7fd9018e619f114bdd3b049cf6968ccfba
spanishprisoner <jacek.mowinski@gmail.com>
2019-01-14 17:50:54 +0100

    Adding unsubscribe to update-popup

--- src/LastSeenWeb.AngularFront/ClientApp/src/app/shared/update-popup.component.ts
+++ src/LastSeenWeb.AngularFront/ClientApp/src/app/shared/update-popup.component.ts
@@ -1,19 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { UpdatePopupService } from './update-popup.service';
 import { LastSeenItem } from './lastseenitem';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-update-popup',
   templateUrl: './update-popup.component.html',
   styleUrls: ['./update-popup.component.css']
 })
-export class UpdatePopupComponent implements OnInit {
+export class UpdatePopupComponent implements OnInit, OnDestroy {
   content: LastSeenItem;
+  contentLoaded$: Subscription;
 
   constructor(private updatePopupService: UpdatePopupService) { }
 
   ngOnInit() {
-    this.updatePopupService.contentLoaded.subscribe((e: LastSeenItem) => this.contentLoaded(this, e));
+    this.contentLoaded$ = this.updatePopupService.contentLoaded.subscribe((e: LastSeenItem) => this.contentLoaded(this, e));
+  }
+
+  ngOnDestroy(): void {
+    this.contentLoaded$.unsubscribe();
   }
 
   contentLoaded(me: UpdatePopupComponent, content: LastSeenItem) {
