895b3a91627e54a58507d8e7fd96d9b99cbae5dc
spanishprisoner <jacek.mowinski@gmail.com>
2019-01-22 18:11:51 +0100

    Adding identityserver4

---
+++ src/LastSeenWeb.Data.Identity/Config.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using IdentityServer4.Models;
+
+namespace LastSeenWeb.Data.Identity
+{
+	public class Config
+	{
+		public static IEnumerable<IdentityResource> GetIdentityResources()
+		{
+			return new List<IdentityResource>
+			{
+			};
+		}
+
+		public static IEnumerable<ApiResource> GetApiResources()
+		{
+			return new List<ApiResource>
+			{
+				new ApiResource("lastseenapi", "Last Seen API")
+			};
+		}
+
+		public static IEnumerable<Client> GetClients(string clientSecret)
+		{
+			return new List<Client>
+			{
+				new Client
+				{
+					ClientId = "lastseen",
+					AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
+					ClientSecrets =
+					{
+						new Secret(clientSecret.Sha256())
+					},
+					AccessTokenLifetime = (int) TimeSpan.FromHours(24).TotalSeconds,
+					AllowedScopes = {
+						"lastseenapi"
+					}
+				}
+			};
+		}
+	}
+}
