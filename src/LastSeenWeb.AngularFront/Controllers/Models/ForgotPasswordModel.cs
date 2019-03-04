b2bf48de120fb62ed2fed40353dd5b398086b4b5
spanishprisoner <jacek.mowinski@gmail.com>
2019-03-04 18:11:06 +0100

    ForgotPasswordComponent

---
+++ src/LastSeenWeb.AngularFront/Controllers/Models/ForgotPasswordModel.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LastSeenWeb.AngularFront.Controllers.Models
+{
+	public class ForgotPasswordModel
+	{
+		public string Email { get; set; }
+	}
+}
---
+++ src/LastSeenWeb.AngularFront/Controllers/Models/ForgotPasswordResponse.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace LastSeenWeb.AngularFront.Controllers.Models
+{
+	public class ForgotPasswordResponse
+	{
+		[JsonProperty(PropertyName = "error")]
+		public string Error { get; set; }
+	}
+}
