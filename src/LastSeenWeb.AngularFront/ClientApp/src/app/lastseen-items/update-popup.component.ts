0038b041a7307097b684f8358139830024599b38
spanishprisoner <jacek.mowinski@gmail.com>
2019-03-19 19:26:35 +0100

    Query flow

--- src/LastSeenWeb.AngularFront/ClientApp/src/app/lastseen-items/update-popup.component.ts
+++ src/LastSeenWeb.AngularFront/ClientApp/src/app/lastseen-items/update-popup.component.ts
@@ -24,8 +24,7 @@ export class UpdatePopupComponent implements OnInit, OnDestroy {
     seconds: [0],
     moveToTop: [true],
     name: [''],
-    imageUrl: [''],
-    remove: [false]
+    imageUrl: ['']
   });
 
   constructor(private updatePopupService: UpdatePopupService,
@@ -41,19 +40,36 @@ export class UpdatePopupComponent implements OnInit, OnDestroy {
     this.contentLoadedSubscription.unsubscribe();
   }
 
-  contentLoaded(me: UpdatePopupComponent, content: LastSeenItem) {
+  contentLoaded(_: UpdatePopupComponent, content: LastSeenItem) {
+    this.form.controls.id.setValue(content.id);
+    this.form.controls.season.setValue(content.season);
+    this.form.controls.episode.setValue(content.episode);
+    this.form.controls.visitUrl.setValue(content.visitUrl);
+    this.form.controls.notes.setValue(content.notes);
+    this.form.controls.unfinished.setValue(content.unfinished);
+    this.form.controls.hours.setValue(content.hours);
+    this.form.controls.minutes.setValue(content.minutes);
+    this.form.controls.seconds.setValue(content.seconds);
+    this.form.controls.moveToTop.setValue(content.moveToTop);
+    this.form.controls.name.setValue(content.name);
+    this.form.controls.imageUrl.setValue(content.imageUrl);
+
     for (let i = 2; i <= 4; i++) {
       if (this.$('#collapse' + i).hasClass('in')) {
         this.$('#collapse' + i).collapse('toggle');
       }
     }
-    me.updatePopupService.open();
+    this.updatePopupService.open();
     if (this.$('#collapse1').hasClass('in') === false) {
       this.$('#collapse1').collapse('toggle');
     }
   }
 
   remove() {
+    if (!this.form.controls.id.value) {
+      return;
+    }
+    console.log('remove' + this.form.controls.id.value);
   }
 
   onSubmit() {
