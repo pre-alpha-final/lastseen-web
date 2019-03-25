bbfac3ec7c8c62e8ad4c05d13e02fc500b8e9ba7
spanishprisoner <jacek.mowinski@gmail.com>
2019-03-25 19:18:26 +0100

    Moving to production data

--- src/LastSeenWeb.AngularFront/Controllers/LastSeenItemsController.cs
+++ src/LastSeenWeb.AngularFront/Controllers/LastSeenItemsController.cs
@@ -1,6 +1,6 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
+using LastSeenWeb.AngularFront.Controllers.Models;
 using LastSeenWeb.Core.Services;
 using LastSeenWeb.Domain.Models;
 using Microsoft.AspNetCore.Authorization;
@@ -19,39 +19,58 @@ namespace LastSeenWeb.AngularFront.Controllers
 			_lastSeenService = lastSeenService;
 		}
 
-		[HttpGet("")]
+		[HttpPut]
+		public async Task<IActionResult> Upsert([FromBody] LastSeenItemModel model)
+		{
+			var domain = new LastSeenItem
+			{
+				Id = model.Id,
+				Season = model.Season,
+				Episode = model.Episode,
+				VisitUrl = model.VisitUrl,
+				Unfinished = model.Unfinished,
+				Hours = model.Hours,
+				Minutes = model.Minutes,
+				Seconds = model.Seconds,
+				Notes = model.Notes,
+				MoveToTop = model.MoveToTop,
+				Name = model.Name,
+				ImageUrl = model.ImageUrl,
+				TrackingUrl = model.TrackingUrl,
+				EpisodesBehind = model.EpisodesBehind,
+			};
+			await _lastSeenService.Upsert(domain, GetUsername());
+
+			return Ok();
+		}
+
+		[HttpGet]
 		public async Task<IActionResult> GetAll()
 		{
-			//var items = await _lastSeenService.GetAll(HttpContext.User.Claims
-			//	.FirstOrDefault(e => e.Type == "username")?.Value ?? "");
+			var items = await _lastSeenService.GetAll(GetUsername());
 
-			//return Ok(items);
-
-			return Ok(_stub);
+			return Ok(items);
 		}
 
 		[HttpGet("{id}")]
 		public async Task<IActionResult> Get(string id)
 		{
-			return Ok(_stub.FirstOrDefault(e => e.Id == id));
+			var item = await _lastSeenService.Get(id, GetUsername());
+
+			return Ok(item);
 		}
 
 		[HttpDelete("{id}")]
 		public async Task<IActionResult> Delete(string id)
 		{
-			_stub.Remove(_stub.Where(e => e.Id == id).FirstOrDefault());
-			return Ok();
+			await _lastSeenService.Delete(id, GetUsername());
+
+			return NoContent();
 		}
 
-		private static List<LastSeenItem> _stub = new List<LastSeenItem>
+		private string GetUsername()
 		{
-			new LastSeenItem { Id = "1", Name = "item 1", Season = 1, Unfinished = true, Hours = 2, Minutes = 3, Notes = "some notes", VisitUrl = "http://google.com", ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/SNice.svg/1200px-SNice.svg.png" },
-			new LastSeenItem { Id = "2", Name = "item 2", Season  = 1, Episode = 1, ImageUrl = "https://images-na.ssl-images-amazon.com/images/I/51zLZbEVSTL._SX425_.jpg" },
-			new LastSeenItem { Id = "3", Name = "item", Season  = 1, Episode = 1, ImageUrl = "https://i.guim.co.uk/img/static/sys-images/Arts/Arts_/Pictures/2009/2/20/1235150177056/Smiley-001.jpg?width=300&quality=85&auto=format&fit=max&s=5cf88208cce59af2cf03b827227efdff" },
-			new LastSeenItem { Id = "4", Name = "item 3", Episode = 1, Notes = "some notes", VisitUrl = "http://google.com", ImageUrl = "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/SNice.svg/220px-SNice.svg.png" },
-			new LastSeenItem { Id = "5", Name = "item 4", Unfinished = true, Hours = 1, ImageUrl = "https://spectator.imgix.net/content/uploads/2015/06/Emoji.jpg?auto=compress,enhance,format&crop=faces,entropy,edges&fit=crop&w=620&h=413" },
-			new LastSeenItem { Id = "6", Name = "item 5", Unfinished = true, Minutes = 1, VisitUrl = "", ImageUrl = "https://cdn.shopify.com/s/files/1/1061/1924/products/Smiling_Face_Emoji_with_Blushed_Cheeks_large.png?v=1480481056" },
-			new LastSeenItem { Id = "7", Name = "item 6", Unfinished = true, Hours = 1, Minutes = 2, ImageUrl = "https://images-na.ssl-images-amazon.com/images/I/41q0g8iq5iL.jpg" }
-		};
+			return HttpContext.User.Claims.FirstOrDefault(e => e.Type == "username")?.Value ?? "";
+		}
 	}
 }
